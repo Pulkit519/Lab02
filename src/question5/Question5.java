f4a0c93fa5a2b7c8c26747f7620cf79641e68336
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-08-19 15:18:27 +0530

    add (character1, char)

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        char character1 = scanner.nextLine().trim().charAt(0);
+        char character2 = scanner.next().trim().charAt(0);
+
+
 
     }
 }
