7a274d7db0b051f6a064ed162663bf74254a0879
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-08-19 15:10:04 +0530

    program build up

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        if (value1 > 0) {
+            System.out.println("Positive");
+        } else if (value1 == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Nagative");
+        }
 
     }
 }
